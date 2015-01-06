91acb746a36d85b1cc02936dfd2bea652f1bfb42
David Lonjon <david.lonjon@gmail.com>
2015-01-06 14:50:30 +0900

    Add command to install ruby and composer dependencies

--- config/prepare.sh
+++ config/prepare.sh
@@ -3,5 +3,12 @@ git init
 rm -rf wordpress
 git submodule add -b 4.0-branch https://github.com/WordPress/WordPress.git wordpress
 git remote rm origin
+
+echo "install Ruby dependencies"
+bundle install
+
+echo "Install php packages via composer"
+composer install
+
 git add -A
 git commit -m "Inital commit"
